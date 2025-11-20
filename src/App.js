73d4ff6ac6cae3b90b2178fea9f65e3f889b9d84
Daniel <general@disaksen.dev>
2025-11-20 22:03:57 +0000

    uninstalled craco and switched to tailwind cdn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="text-4xl font-bold text-red-500 p-10">
+      Tailwind Working?
+    </div>
+  );
 }
 
 export default App;
+
