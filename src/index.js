5462db9917eb3ded17a6d55afe6452d11469b5c4
Daniel <general@disaksen.dev>
2025-11-21 23:12:53 -0500

    added files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -18,4 +5,3 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
->>>>>>> 3555250413ae27d7c094633e5ce6035e165c693d
